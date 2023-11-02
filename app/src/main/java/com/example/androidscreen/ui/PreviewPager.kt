116e3356c25a2d6e1bd9443fc7efaa4041134c28
Dmitry Kargapolov <mr.kargapolov@gmail.com>
2023-11-02 14:37:07 +0600

    Добавил иконку play на картинку в карусели

--- app/src/main/java/com/example/androidscreen/ui/PreviewPager.kt
+++ app/src/main/java/com/example/androidscreen/ui/PreviewPager.kt
@@ -2,6 +2,7 @@ package com.example.androidscreen.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -10,15 +11,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.androidscreen.R
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -37,20 +46,53 @@ fun VideoPreviewRow(
             pageSpacing = -(50).dp,
         ) {
                 index ->
-            Image(
-                painter = painterResource(id = previewResList[index]),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(270.dp)
-                    .aspectRatio(16f / 9f)
-                    .clip(RoundedCornerShape(15.dp)),
-                colorFilter =
-                ColorFilter.colorMatrix(
-                    ColorMatrix()
-                    .apply{setToScale(
-                        0.6f,0.6f,0.6f,1f)}),
-            )
+            Box{
+                Image(
+                    painter = painterResource(id = previewResList[index]),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(270.dp)
+                        .aspectRatio(16f / 9f)
+                        .clip(RoundedCornerShape(15.dp))
+                        .background(Color(0x77000000))
+                        .graphicsLayer {
+                            shape = CircleShape
+                            alpha = 0.7f
+                        },
+                    colorFilter =
+                    ColorFilter.colorMatrix(
+                        ColorMatrix()
+                            .apply {
+                                setToScale(
+                                    0.6f, 0.6f, 0.6f, 1f
+                                )
+                            }),
+
+                    )
+                if (index == 0) {
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(54.dp)
+                            .clip(CircleShape)
+                            .blur(40.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Gray,
+                            contentColor = Color.Black
+                        ),
+                    ) {}
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(13.dp)
+
+                    )
+                }
+            }
         }
 
     }
